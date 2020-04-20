fb945ada128221f302811115c2dac22240120a41
xudongye <simonye@aliyun.com>
2020-04-20 15:21:33 +0800

    package部署128服务器

--- YoungchJavaPat/pat/pat-web/src/main/java/com/youngch/pat/PatApplication.java
+++ YoungchJavaPat/pat/pat-web/src/main/java/com/youngch/pat/PatApplication.java
@@ -1,6 +1,6 @@
 package com.youngch.pat;
 
-import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -12,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @EnableCaching
 @ComponentScan({"com.youngch.*"})
-@MapperScan("com.youngch.pat.dao")
 @SpringBootApplication
 public class PatApplication {
 
